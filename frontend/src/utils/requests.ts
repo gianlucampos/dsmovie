88d894fc7d8279d26d1a776ef19d5ee2420963fc
Gianluca Campos <gianlucampos97@gmail.com>
2022-01-15 17:21:08 -0300

    useState, useEffect

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+let local = "http://localhost:8080";
+let prod = "https://dsmovie-gianlucampos.herokuapp.com";
+
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? prod;
