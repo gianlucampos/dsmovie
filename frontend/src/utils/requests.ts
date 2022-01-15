d11f93d4859c9a27f74bc2735f199b0105e9a64a
Gianluca Campos <gianlucampos97@gmail.com>
2022-01-15 20:17:01 -0300

    Corrigindo endpoint de produção

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,1 @@
-let local = "http://localhost:8080";
-let prod = "https://dsmovie-gianlucampos.herokuapp.com";
-
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? prod;
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
